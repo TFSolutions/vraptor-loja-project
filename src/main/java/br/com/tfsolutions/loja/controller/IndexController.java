5e2e2f2158f364cd4f351ee797e106c815b89177
Filipe Monteiro <felipedelphioracle@gmail.com>
2013-02-19 12:46:07 -0300

    Ajustando configuração inicial do projeto

---
+++ src/main/java/br/com/tfsolutions/loja/controller/IndexController.java
@@ -0,0 +1,18 @@
+package br.com.tfsolutions.loja.controller;
+
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class IndexController {
+  
+  public IndexController(){
+    
+  }
+  
+  @Path("/")
+  public void index(){
+    
+  }
+  
+}
